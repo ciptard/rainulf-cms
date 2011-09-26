456ba402c0a60bc67f4ac39c8337e3adf3b9e18b
Rainulf Pineda <rainulf@rainulf.ca>
2011-09-26 19:39:24 -0400

    Completely separated HTML source from index.php to created _template/default.php. Copied contents of controller.inc.php to index.php and removed controller.inc.php

---
+++ _template/default.php
@@ -0,0 +1,109 @@
+<?php
+if(!defined("INDEX")) die("Not allowed.");
+?>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<head>
+   <title><?php echo $indexTitle; ?></title>
+   <meta name="description" content="<?php echo $indexDesc; ?>" />
+   <meta name="keywords" content="<?php echo $indexKeyw; ?>" />
+   <meta name="author" content="<?php echo SITE_AUTHOR; ?>" />
+   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+   <link rel="stylesheet" type="text/css" href="style.css" />
+   <link rel="shortcut icon" href="favicon.ico" />
+</head>
+
+
+<body>
+   <div id="container">
+   
+   
+      <div id="header">
+         <h1><a href="."><?php echo SITE_TITLE; ?></a></h1>
+
+         <h2 id="quote"></h2><br />
+         <hr />
+      </div><!-- end header -->
+
+      <div id="left">
+      
+      
+         <h3>Latest Entries</h3>
+         <ul>
+         <?php foreach($indexEntries as $one): ?>
+            <li><a href='<?php echo Helper::titleToHTMLExt($one->id, $one->Title); ?>'><?php echo $one->Title; ?></a></li>
+         <?php endforeach; ?>
+         </ul>
+         
+         
+         <h3>Tags</h3>
+         <ul>
+            <li>
+               <?php foreach($indexTags as $one): ?>
+                  <a href='<?php echo $one['name']; ?>tag.html'><?php echo $one['name']; ?></a> [<?php echo $one['count']; ?>]
+               <?php endforeach; ?>
+            </li>
+         </ul>
+         
+         
+         <h3>Links</h3>
+         <ul>
+            <li><a href="http://zenit.senecac.on.ca/wiki/index.php/User:Rainulf" target="_blank">About Rainulf</a></li>
+            <li><a href="http://twitter.com/rainulf" target="_blank">Rainulf@Twitter</a></li>
+            <li><a href="https://github.com/rainulf" target="_blank">Rainulf@github</a></li>
+            <li><a href="http://bit.ly/rainulfirc" target="_blank">Rainulf@IRC Freenode</a></li>
+            <li><a href="http://helloyounha.com/xe/" target="_blank">Hello!Younha!</a></li>
+            <li><a href="http://www.animenewsnetwork.com/" target="_blank">Anime News Network</a></li>
+            <li><a href="http://myanimelist.net/" target="_blank">MyAnimeList</a></li>
+            <li><a href="http://randomc.net/" target="_blank">Random Curiosity</a></li>
+            <li><a href="https://my.senecacollege.ca/" target="_blank">Seneca BlackBoard</a></li>
+            <li><a href="https://scs.senecac.on.ca/" target="_blank">Seneca CS</a></li>
+            <li><a href="https://learn.senecac.on.ca/" target="_blank">Seneca Webmail</a></li>
+         </ul>
+         
+         
+      </div><!-- end left division -->
+
+      <div id="main">
+      <?php foreach($indexPosts as $one): ?>
+         <a id='<?php echo $one->id; ?>'></a>
+         <div class='post'>
+            <h1 class='title'><a href="javascript:unhide('id_<?php echo $one->id; ?>');"><?php echo $one->Title; ?></a></h1>
+            <div id='id_<?php echo $one->id; ?>' class='postContents'>
+               <p class='byline'><small>Posted on <?php echo $one->PostD; ?></small></p>
+               <div class='entry'>
+               <?php echo $one->content; ?>
+               </div>
+               <div class="postComments"></div>
+               <div class="meta">
+                  <p class="links"><a href='#'>Back to top</a> &nbsp;&bull;&nbsp; <a class="permalink" href='<?php echo Helper::titleToHTMLExt($one->id, $one->Title); ?>'>Permalink</a></p>
+               </div>
+               
+            </div>
+         </div>
+      <?php endforeach; ?>
+      </div>
+      
+      <div id="footer">
+         <hr />
+         <p class="left">| <a href=
+             "http://jigsaw.w3.org/css-validator/">CSS</a> | <a href=
+             "http://validator.w3.org/check?uri=referer">HTML5</a>
+         |</p>
+         <p class="right">With Glowing Hearts.</p>
+      </div><!-- end footer -->
+      
+      
+   </div><!-- end container -->
+   
+   
+   <script src='./_js/jquery-1.5.2.min.js' type='text/javascript'></script>
+   <script src='./_js/script.js' type='text/javascript'></script>
+   
+   
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,112 +1,64 @@
 <?php
+/******************************
+ * Author      : Rainulf      *
+ * Date Started: Sep 26, 2011 *
+ * Last Updated: Sep 26, 2011 *
+ ******************************/
 
-define("INDEX",TRUE);
-require_once './controller.inc.php';
+define("INDEX", true);
+
+error_reporting(-1);
+session_start();
+date_default_timezone_set('America/Toronto');
+require_once './conf.php';
+require_once './helpers.php';
+require_once './_classes/DatabaseConnection.php';
+require_once './_classes/ContentsModel.php';
+require_once './_classes/ContentsModelMapper.php';
+
+Helper::fixMagicQuotes();
+$indexTitle = SITE_TITLE;
+$indexDesc = SITE_DESC;
+$indexKeyw = SITE_KEYW;
+
+/**
+ * Mapper creation and REQUEST grabbing
+ */
+$mapper = new ContentsModelMapper();
+$mapper->SetOrder('PostD', 'DESC');
+
+if(isset($_GET['tags'])){
+   $indexPosts = $mapper->Fetch('Tags', '%'.strip_tags(htmlspecialchars($_GET['tags'])).'%');
+   $indexTitle = "Tag: " . strip_tags(htmlspecialchars($_GET['tags'])) . " - " . $indexTitle;
+}
+else if(isset($_GET['id'])){
+   $indexPosts = $mapper->Fetch('id', intval($_GET['id']), true);
+   $indexTitle = $indexPosts[0]->Title . " - " . $indexTitle;
+   $indexDesc = trim(strip_tags(substr($indexPosts[0]->content, 0, 150))) . '...';
+   $indexKeyw = Helper::generateMetaKeywords($indexPosts[0]->content);
+}
+else {
+   // TODO: Paging
+   $indexPosts = $mapper->FetchAll(0, CONTENTS_PER_PG);
+}
+               
+if(!$mapper->IsEmpty()){
+   $mapper->EmptyModelList();
+}
+$indexEntries = $mapper->FetchColumn('id, Title', true);
+if(!$mapper->IsEmpty()){
+   $mapper->EmptyModelList();
+}
+$fetchedTags = $mapper->FetchColumn('Tags');
+foreach($fetchedTags as $tag){
+   if(!empty($tag->Tags)){
+      $arrTags[] = $tag->Tags;
+   }
+}
+$indexTags = Helper::tagsToHTMLExt($arrTags);
+
+
+
+include './_template/default.php';
 
 ?>
-
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-
-<head>
-   <title><?php echo $indexTitle; ?></title>
-   <meta name="description" content="<?php echo $indexDesc; ?>" />
-   <meta name="keywords" content="<?php echo $indexKeyw; ?>" />
-   <meta name="author" content="<?php echo SITE_AUTHOR; ?>" />
-   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-   <link rel="stylesheet" type="text/css" href="style.css" />
-   <link rel="shortcut icon" href="favicon.ico" />
-</head>
-
-
-<body>
-   <div id="container">
-   
-   
-      <div id="header">
-         <h1><a href="."><?php echo SITE_TITLE; ?></a></h1>
-
-         <h2 id="quote"></h2><br />
-         <hr />
-      </div><!-- end header -->
-
-      <div id="left">
-      
-      
-         <h3>Latest Entries</h3>
-         <ul>
-         <?php foreach($indexEntries as $one): ?>
-            <li><a href='<?php echo Helper::titleToHTMLExt($one->id, $one->Title); ?>'><?php echo $one->Title; ?></a></li>
-         <?php endforeach; ?>
-         </ul>
-         
-         
-         <h3>Tags</h3>
-         <ul>
-            <li>
-               <?php foreach($indexTags as $one): ?>
-                  <a href='<?php echo $one['name']; ?>tag.html'><?php echo $one['name']; ?></a> [<?php echo $one['count']; ?>]
-               <?php endforeach; ?>
-            </li>
-         </ul>
-         
-         
-         <h3>Links</h3>
-         <ul>
-            <li><a href="http://zenit.senecac.on.ca/wiki/index.php/User:Rainulf" target="_blank">About Rainulf</a></li>
-            <li><a href="http://twitter.com/rainulf" target="_blank">Rainulf@Twitter</a></li>
-            <li><a href="https://github.com/rainulf" target="_blank">Rainulf@github</a></li>
-            <li><a href="http://bit.ly/rainulfirc" target="_blank">Rainulf@IRC Freenode</a></li>
-            <li><a href="http://helloyounha.com/xe/" target="_blank">Hello!Younha!</a></li>
-            <li><a href="http://www.animenewsnetwork.com/" target="_blank">Anime News Network</a></li>
-            <li><a href="http://myanimelist.net/" target="_blank">MyAnimeList</a></li>
-            <li><a href="http://randomc.net/" target="_blank">Random Curiosity</a></li>
-            <li><a href="https://my.senecacollege.ca/" target="_blank">Seneca BlackBoard</a></li>
-            <li><a href="https://scs.senecac.on.ca/" target="_blank">Seneca CS</a></li>
-            <li><a href="https://learn.senecac.on.ca/" target="_blank">Seneca Webmail</a></li>
-         </ul>
-         
-         
-      </div><!-- end left division -->
-
-      <div id="main">
-      <?php foreach($indexPosts as $one): ?>
-         <a id='<?php echo $one->id; ?>'></a>
-		   <div class='post'>
-			   <h1 class='title'><a href="javascript:unhide('id_<?php echo $one->id; ?>');"><?php echo $one->Title; ?></a></h1>
-			   <div id='id_<?php echo $one->id; ?>' class='postContents'>
-			      <p class='byline'><small>Posted on <?php echo $one->PostD; ?></small></p>
-			      <div class='entry'>
-               <?php echo $one->content; ?>
-			      </div>
-               <div class="postComments"></div>
-			      <div class="meta">
-				      <p class="links"><a href='#'>Back to top</a> &nbsp;&bull;&nbsp; <a class="permalink" href='<?php echo Helper::titleToHTMLExt($one->id, $one->Title); ?>'>Permalink</a></p>
-			      </div>
-               
-			   </div>
-		   </div>
-      <?php endforeach; ?>
-      </div>
-      
-      <div id="footer">
-         <hr />
-         <p class="left">| <a href=
-             "http://jigsaw.w3.org/css-validator/">CSS</a> | <a href=
-             "http://validator.w3.org/check?uri=referer">HTML5</a>
-         |</p>
-         <p class="right">With Glowing Hearts.</p>
-      </div><!-- end footer -->
-      
-      
-   </div><!-- end container -->
-   
-   
-   <script src='./_js/jquery-1.5.2.min.js' type='text/javascript'></script>
-   <script src='./_js/script.js' type='text/javascript'></script>
-   
-   
-</body>
-</html>
