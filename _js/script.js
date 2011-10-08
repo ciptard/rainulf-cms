7eefedc3a5c5d8f6a6462ca9e4171a95841b0d14
Rainulf Pineda <rainulf@rainulf.ca>
2011-10-08 04:12:20 -0400

    Added #11 - added content auto-load when page bottom is reached

--- _js/script.js
+++ _js/script.js
@@ -3,6 +3,12 @@
  * Date Started: Oct 31, 2010 *
  * Last Updated: Mar 13, 2011 *
  ******************************/
+// GLOBAL VARIABLES
+//
+// Used for auto paging
+var nextOffset = 7;
+// Lock
+var isLocked = false;
 
 // FUNCTIONS
 //
@@ -13,37 +19,16 @@ function generateRandom(numberOfQuotes) {
 
 
 function unhide(divID) {
-/*
-   var item = document.getElementById(divID);
-   if (item) {
-      item.className=(item.className=='hidden')?'unhidden':'hidden';
-   }
-*/
-   $(".postContents").slideUp();
-   $(".postContents .postComments").empty();
-   var permalink = "http://rainulf.ca/" + $("#" + divID + " .meta .links .permalink").attr("href");
-   var disqusHTML = generateDisqusHTML(permalink);
-   $("#" + divID + " .postComments").html(disqusHTML);
-   
-   $("#" + divID).slideToggle("slow");   
-}
-
-function pageAction(pageNum, maxPageNum, obj) {
-   pageNum = pageNum - 0;
-   $('#cur_page_num').text(pageNum);
-   if(pageNum <= 1) {
-      $('a#prev_page').removeAttr('href');
-   }
-   else {
-      $('a#prev_page').attr('href', "?page=" + eval(pageNum - 1));
-   }
-   if(pageNum < maxPageNum) {
-      $('a#next_page').attr('href', "?page=" + eval(pageNum + 1));
-   }
-   else {
-      $('a#next_page').removeAttr('href');
-   }
-   $(obj).fadeIn();
+   isLocked = true;
+   $(document).ready(function(){
+      $(".postContents").slideUp();
+      $(".postContents .postComments").empty();
+      $("#" + divID).slideToggle("slow", function(){ window.location.hash=divID; isLocked = false;}); 
+      
+      var permalink = "http://rainulf.ca/" + $("#" + divID + " .meta .links .permalink").attr("href");
+      var disqusHTML = generateDisqusHTML(permalink);
+      $("#" + divID + " .postComments").html(disqusHTML);
+   });
 }
 
 function generateDisqusHTML(url){
@@ -77,56 +62,17 @@ $(document).ready(function(){
    var permalink = "http://rainulf.ca/" + $(".postContents:first .meta .links .permalink").attr("href");
    var disqusHTML = generateDisqusHTML(permalink);
    $(".postContents:first .postComments").html(disqusHTML);
-
-   // Instant Search
-   $("#search_bar").keyup(function(event){
-      var str = $(this).val( );
-      // must only trigger if letter or backspace
-      if(event.which >= 65 && event.which <= 90 || event.which == 8) { 
-         if (str.length < 3) { 
-            var need = 3 - str.length;
-            var wordcharacter = (need > 1) ? "characters" : "character";
-            $('#content').html("<p>Please enter " + need + " more " + wordcharacter + " to start. <br /><br />You are searching for '" + str + "'... </p>");
-         }
-         else {
-            // $('#content').html('<p><img src="./images/loading-gif-animation.gif" /></p>'); TODO: make this work
-            $('#content').hide( ).load("show.php?search_bar="+str).fadeIn('slow');
-         }
-      }
-   });
    
-   // Paging
-   $('a#next_page').click(function() {
-      var pageLink = $('a#next_page').attr('href');
-      var varSplitted = pageLink.split('=');
-      var pageNum  = varSplitted[1];
-      var maxPageNum = $('span#max_page_num').text( );
-      $('#content').hide( ).load("show.php" + pageLink, function(response, status, xhr) {
-         if (status == "error") {
-           // TODO: handle error
-         }
-         else {
-            pageAction(pageNum, maxPageNum, this);
-         }
-      });
-      return false; // Disable link
-   });
-
-   $('a#prev_page').click(function() {
-      var pageLink = $('a#prev_page').attr('href');
-      var varSplitted = pageLink.split('=');
-      var pageNum  = varSplitted[1];
-      var maxPageNum = $('span#max_page_num').text( );
-      $('#content').hide( ).load("show.php" + pageLink, function(response, status, xhr) {
-         if (status == "error") {
-            // TODO: handle error
-         }
-         else {
-            pageAction(pageNum, maxPageNum, this);
-         }
-      });
-      return false; // Disable link
-   });
+   // Content load at bottom (paging replacement)
+   $(window).scroll(function(){
+      if($(window).scrollTop() == $(document).height() - $(window).height() && !isLocked){
+         $.get('./getcontent.php', { offset: nextOffset }, function(data) {
+            $("#main").append(data);
+            $(".appended").hide().removeClass("appended");
+            nextOffset += 7;
+         });
+      }
+   }); 
    
 });
 
---
+++ getcontent.php
@@ -0,0 +1,43 @@
+<?php
+define("INDEX", true);
+
+error_reporting(-1);
+session_start();
+date_default_timezone_set('America/Toronto');
+require_once './conf.php';
+require_once './helpers.php';
+require_once './_classes/DatabaseConnection.php';
+require_once './_classes/ContentsModel.php';
+require_once './_classes/ContentsModelMapper.php';
+
+Helper::fixMagicQuotes();
+/**
+ * Mapper creation and REQUEST grabbing
+ */
+$mapper = new ContentsModelMapper();
+$mapper->SetOrder('PostD', 'DESC');
+
+$requests = Helper::getRequests();
+
+$offset = intval($requests['offset']);
+$indexPosts = $mapper->FetchAll($offset, CONTENTS_PER_PG);
+
+?>
+
+      <?php foreach($indexPosts as $one): ?>
+         <a id='<?php echo $one->id; ?>'></a>
+         <div class='post'>
+            <h1 class='title'><a href="javascript:unhide('id_<?php echo $one->id; ?>');"><?php echo $one->Title; ?></a></h1>
+            <div id='id_<?php echo $one->id; ?>' class='postContents appended'>
+               <p class='byline'><small>Posted on <?php echo Helper::formatDate($one->PostD); ?></small></p>
+               <div class='entry'>
+               <?php echo $one->content; ?>
+               </div>
+               <div class="postComments"></div>
+               <div class="meta">
+                  <p class="links"><a href='#'>Back to top</a> &nbsp;&bull;&nbsp; <a class="permalink" href='<?php echo Helper::titleToHTMLExt($one->id, $one->Title); ?>'>Permalink</a></p>
+               </div>
+               
+            </div>
+         </div>
+      <?php endforeach; ?>
