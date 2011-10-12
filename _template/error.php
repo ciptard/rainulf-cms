54cdc79c0a92bda3c58dcd5dadead7282a28fc5d
Rainulf Pineda <rainulf@rainulf.ca>
2011-10-11 20:47:15 -0400

    Fixed HTML5 error - no alt attribute.

--- _template/error.php
+++ _template/error.php
@@ -68,7 +68,7 @@ if(!defined("INDEX")) die("Not allowed.");
          
          <h3>Certifications</h3>
          <ul id="certs">
-            <li><a href="http://www.refsnesdata.no/certification/w3certified.asp?id=3055467"><img src="./_images/w3cert.png" border="0" /></a></li>
+            <li><a href="http://www.refsnesdata.no/certification/w3certified.asp?id=3055467"><img src="./_images/w3cert.png" alt="w3cert" style="border: 0;" /></a></li>
          </ul>
          
          
