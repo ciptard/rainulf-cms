ce7cbec9b70bac0544e96ac2593a01efe3f797dd
Rainulf Pineda <rainulf@rainulf.ca>
2011-11-05 02:21:12 -0400

    Added a conf.php sample

---
+++ conf_sample.php
@@ -0,0 +1,34 @@
+<?php
+/******************************
+ * Author      : Rainulf      *
+ * Date Started: Oct 19, 2010 *
+ * Last Updated: Feb 14, 2011 *
+ ******************************/
+
+if(!defined("INDEX")) die("Not allowed.");
+/**
+ * Website info (usually for meta tags)
+ */
+define('SITE_TITLE', 'Rainulf');
+define('SITE_DESC', 'Jose Rainulf Pineda\'s official website. Software and Web development - C/C++, C#, Java, PHP, ASP.NET, mySQL, xHTML and JavaScript languages.');
+define('SITE_KEYW', 'jose rainulf pineda, rainulf\'s website, open source, software development, web development, c++ and c, php, mysql, xhtml, javascript, programming languages, programming tutorials, anime, manga');
+define('SITE_AUTHOR', 'Rainulf Pineda');
+define('SITE_URL', 'http://rainulf.ca/');
+define('SITE_URL_HTTPS', 'https://rainulf.ca/');
+define('CONTENTS_PER_PG', '7');
+
+/**
+ * Database info
+ */
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+define('DB_HOST', '');
+define('DB_CHARSET', '');
+
+/**
+ * Misc
+ */
+define('SELF_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+
+?>
