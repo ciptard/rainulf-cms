f4359e1cf9557586ab83aa2ca2511423c91f4838
Rainulf Pineda <rainulf@rainulf.ca>
2011-11-16 23:08:41 -0500

    draft for url redirector

--- _template/r.php
+++ _template/r.php
@@ -8,7 +8,12 @@ if(!defined("INDEX")) die("Not allowed.");
 
 
       <div id="main">
-
+         <form action="" method="get">
+            <p>URL to redirect: <input type="text" name="url" /><input type="submit" value="Go!" /></p>
+         <?php if(isset($shortUrl)): ?>
+            <p>Short URL: <input type="text" name="shorturl" value="<?php echo $shortUrl; ?>" /></p>
+         <?php endif; ?>
+         </form>
       </div>
 
 <?php include Helper::getPath(__FILE__) . 'foot.php'; ?>
--- r/index.php
+++ r/index.php
@@ -18,22 +18,47 @@ $jsOut = '';
 /**
  * Mapper creation and REQUEST grabbing
  */
-$mapper = new ContentsModelMapper();
-$mapper->SetOrder('PostD', 'DESC');
+$mysqlMapper = new ContentsModelMapper();
+$mysqlMapper->SetOrder('PostD', 'DESC');
+$mongodb = new MongoDBConnection();
+$mongodb->setCollection('urlredirector');
+$mongodb->setOrder('VisitDate', 'DESC');
+
 $requests = Helper::getRequests();
-if(array_key_exists('tags', $requests)){
-   $indexPosts = $mapper->Fetch('Tags', '%'.$requests['tags'].'%');
-   $indexTitle = "Tag: " . $requests['tags'] . " - " . $indexTitle;   
-   $jsOut .= 'scrollLock = true;';
-}              
-$indexEntries = $mapper->FetchColumn('id, Title', true);
-$fetchedTags = $mapper->FetchColumn('Tags');
+             
+$indexEntries = $mysqlMapper->FetchColumn('id, Title', true);
+$fetchedTags = $mysqlMapper->FetchColumn('Tags');
 if(!empty($fetchedTags)){
    foreach($fetchedTags as $tag){
       !empty($tag->Tags) && $arrTags[] = $tag->Tags;
    }
 }
 $indexTags = Helper::tagsToHTMLExt($arrTags);
+
+if(isset($requests['url']) && Helper::isValidURL($requests['url'])){
+   $url = $requests['url'];
+   $randomStr = Helper::randomStrings();
+   $data = array(
+      'locId' => $randomStr,
+      'url' => $url
+   );
+   $searchData = array('url' => $url);
+   $stuff = $mongodb->insertData($data, $searchData);
+   if($stuff->count() > 0){
+      $stuff->next();
+      $stuff = $stuff->current();
+      $shortUrl = SITE_URL . 'r/to/' . $stuff['locId'];
+   }
+}
+else if(isset($requests['to'])){
+   $searchData = array('locId' => $requests['to']);
+   $stuff = $mongodb->searchData($searchData);
+   if($stuff->count() > 0){
+      $stuff->next();
+      $stuff = $stuff->current();
+      header('Location: ' . $stuff['url']);
+   }
+}
 
 
 include '../_template/r.php';
