84a82c925ac7d4906fe72f5c99f0670c02690d8e
Rainulf Pineda <rainulf@rainulf.ca>
2011-11-16 23:07:40 -0500

    fixed bug in error templates

--- _template/error.php
+++ _template/error.php
@@ -2,12 +2,12 @@
 if(!defined("INDEX")) die("Not allowed.");
 ?>
 
-<?php include SELF_PATH . 'head.php'; ?>
-<?php include SELF_PATH . 'leftdiv.php'; ?> 
+<?php include Helper::getPath(__FILE__) . 'head.php';?>
+<?php include Helper::getPath(__FILE__) . 'leftdiv.php'; ?> 
 
       <div id="main">
          <h1 style="text-align:center;"><?php echo $errorHeader; ?></h1><br />
          <img style="display:block;margin-left:auto;margin-right:auto;"src="<?php echo $errorImg; ?>" alt="<?php echo $errorImgAlt; ?>" />
       </div>
       
-<?php include SELF_PATH . 'foot.php'; ?>
+<?php include Helper::getPath(__FILE__) . 'foot.php'; ?>
