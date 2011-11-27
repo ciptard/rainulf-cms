397ff1940b848988d1eed0517c3b670b52d3fa98
Rainulf Pineda <rainulf@rainulf.ca>
2011-11-27 11:16:09 -0500

    fixed error in rss.php, wrong requires

--- rss.php
+++ rss.php
@@ -9,11 +9,7 @@ define('INDEX', true);
 
 error_reporting(-1);
 date_default_timezone_set('America/Toronto');
-require_once './conf.php';
-require_once './helpers.php';
-require_once './_classes/DatabaseConnection.php';
-require_once './_classes/ContentsModel.php';
-require_once './_classes/ContentsModelMapper.php';
+require_once './commonrequires.php';
 
 
 $mapper = new ContentsModelMapper();
